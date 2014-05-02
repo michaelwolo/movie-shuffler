c15685dffe92246ee0561478f9e0580e48856c9b
Michael Woloszuk <mwoloszuk@gmail.com>
2014-05-02 02:18:33 -0400

    Remove unnecessary code

--- scripts/tags.php
+++ scripts/tags.php
@@ -6,23 +6,14 @@ $mysqli = new mysqli("localhost",$dbuser,$dbpass,$dbname);
 if($mysqli->connect_errno) {
   echo "Connection Failed: " . $mysqli->connect_errno;
 }
-
 $query = "SELECT DISTINCT `ID`,`Tag` FROM `tags` ORDER BY RAND()";
-
 $result = $mysqli->query($query);
-
 $tags = array();
-$index = 0;
-
 while ($row = $result->fetch_assoc()) {
-  $tags[$index] = $row['Tag'];
-	$index++;
+  $tags[] = $row['Tag'];
 };
-
 echo json_encode($tags);
-
 $result->free();
-
 $mysqli->close();
 
 ?>
